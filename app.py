cd051c361d26c46f00ebde12ab90c85bc35d428b
Khalid Omar <khalid.omar@gmail.com>
2020-10-20 16:42:57 +1100

    very minor changes

--- app.py
+++ app.py
@@ -15,11 +15,37 @@ import plotly.express as px
 import matplotlib.pyplot as plt
 
 
-@st.cache
-def get_data() -> Dict[str, pd.DataFrame]:
-    """returns all the datafiles"""
-    df = pd.read_csv("data/cwurData.csv")
-    return df
+def get_data(verbose=False) -> Dict[str, pd.DataFrame]:
+    base_url = "https://github.com/khalido/uni-rankings/raw/main/data/"
+    data_urls = [
+        ("shanghai", "shanghaiData.csv"),
+        ("cwur", "cwurData.csv"),
+        ("times", "timesData.csv"),
+    ]
+
+    country_lookup = pd.read_csv(base_url + "school_and_country_table.csv").rename(
+        columns={"school_name": "university"}
+    )
+
+    data = {}
+    for name, url in data_urls:
+        url = f"data/{url}"
+
+        df = pd.read_csv(url)
+
+        # rename to make col names consistent
+        df = df.rename(
+            columns={"institution": "university", "university_name": "university"}
+        )
+
+        if name == "shanghai":
+            # add country col to shanghai data
+            df = df.merge(country_lookup, on="university", how="left")
+
+        print(f"**{name}** data has {df.shape} values.")
+        data[name] = df
+
+    return data
 
 
 def some_viz(df):
@@ -47,12 +73,34 @@ def par_plot(df):
     return fig
 
 
+def radar_plot(df):
+    all_unis = list(df.institution.unique())
+    unis = st.selectbox("Select a few unis", all_unis)
+
+
+def line_plot(df, N=200, countries=False):
+    """simple line plot of ranking per year"""
+    d = df.query("world_rank <= @N")
+
+    if countries:
+        d = d.query("country == @countries")
+        if d.shape[0] < 1:
+            return "error, selected zero countries"
+
+    fig = px.line(
+        d, x="year", y="world_rank", title="Rank per year", color="university"
+    )
+    fig.update_yaxes(range=[d.world_rank.max() + 1, 0])
+    return fig
+
+
 def main():
     """the starting point of the app"""
     st.title("University Rankings")
 
     st.subheader("Data")
-    df = get_data()
+    data = get_data()
+    df = data["cwur"]
 
     all_countries = list(df.country.unique())
     with st.sidebar:
@@ -78,6 +126,11 @@ def main():
         f"Showing {df.shape[0]} universities from {len(selected_countries)} countries."
     )
     st.dataframe(df)
+
+    st.subheader("Line plot")
+    topN = st.slider("Select top N universities to display", 10, int(df.world_rank.max()), 250)
+    fig = line_plot(df, N=topN)
+    st.plotly_chart(fig)
 
     st.subheader("Some Viz")
     fig = some_viz(df)
